6218c715f4acb5c924c3c829cbffc4e52a634cfc
burakenaktas <business.burakaktas@gmail.com>
2024-03-19 14:30:43 +0100

    style: desktop css fixes

--- src/App.js
+++ src/App.js
@@ -3,31 +3,32 @@ import NavLink from "./components/NavLink";
 import { navLinks } from "./constants/navLinks";
 import Logo from "./assets/logo.svg";
 import LandingPagePicture from "./assets/landing-page-picture.png";
+import Weather from "./components/Weather";
 
 function App() {
   return (
     <div className="App">
-      <div className="h-screen">
-        <div className="bg-primary h-[913px] flex flex-col items-center">
-          <div className="fixed h-[123px] md:h-[203px] w-full flex justify-center bg-primary">
-            <div className="flex flex-col items-left justify-evenly container">
-              <img
-                src={Logo}
-                alt="Construktiv Logo"
-                className="h-12 w-12 md:h-[52px] md:w-[200px]"
-              />
-              <nav className="flex justify-between items-center">
-                <div className="flex space-x-4">
-                  {navLinks.map((link) => (
-                    <NavLink title={link.title} href={link.href} />
-                  ))}
-                </div>
-              </nav>
-            </div>
+      <div className="bg-primary h-[703px] flex flex-col items-center">
+        <div className="fixed h-[123px] md:h-[203px] w-full flex justify-center bg-primary">
+          <div className="flex flex-col items-left justify-evenly container">
+            <img
+              src={Logo}
+              alt="Construktiv Logo"
+              className="h-12 w-12 md:h-[52px] md:w-[200px]"
+            />
+            <nav className="flex justify-between items-center">
+              <div className="flex space-x-4">
+                {navLinks.map((link) => (
+                  <NavLink title={link.title} href={link.href} />
+                ))}
+              </div>
+            </nav>
           </div>
+        </div>
 
-          <div className="container mt-[203px] h-[710px] w-full flex items-center justify-between">
-            <div className="text-left text-secondary text-[70px]">
+        <div className="mt-[203px] h-[710px] w-full flex border-t border-gray-400 flex justify-center">
+          <div className="flex justify-between container">
+            <div className="mt-[124px] text-left text-secondary text-[70px]">
               <h1 className="font-bold">CONSTRUKTIV,</h1>
               <h2>WERBEN IM JETZT</h2>
             </div>
@@ -35,19 +36,36 @@ function App() {
             <img
               src={LandingPagePicture}
               alt="Landing Page"
-              className="rounded-md h-[800px] w-[800px]"
+              className="rounded-[10px] h-[710px] w-[710px]"
             />
           </div>
         </div>
+      </div>
 
-        <div className="flex flex-col h-[400px] w-full items-center justify-center">
-          <div className="w-1/3">
-            <h2 className="text-[40px] text-secondary text-left">
-              <strong>Wilkommen in der Welt des Digital Marketing,</strong> wo
-              wir Ihnen helfen, Ihr Unternehmen zu vergrößern.
-            </h2>
+      <div className="flex flex-col mt-[140px] h-[600px] w-full items-center justify-center">
+        <div className="w-1/3">
+          <h2 className="text-[40px] text-secondary text-left leading-[50px]">
+            <strong>Wilkommen in der Welt des Digital Marketing,</strong> wo wir
+            Ihnen helfen, Ihr Unternehmen zu vergrößern.
+          </h2>
 
-            <p className="text-[#484848] text-[16px] text-left mt-4">
+          <p className="text-[#484848] text-[16px] text-left mt-4">
+            Wir sind ein Team von Experten, die sich auf digitales Marketing
+            spezialisiert haben. Wir bieten Ihnen die besten Lösungen für Ihr
+            Unternehmen und helfen Ihnen,{" "}
+            <strong>Ihre Ziele zu erreichen.</strong> Wir unterstützen Sie bei
+            der Erstellung von Inhalten, der Optimierung Ihrer Website und
+            vielem mehr. Wir sind hier, um Ihnen zu helfen, Ihre Marke zu
+            stärken und Ihr Unternehmen zu vergrößern. Wir freuen uns darauf,
+            mit Ihnen zusammenzuarbeiten.
+          </p>
+        </div>
+      </div>
+
+      <div className="w-full flex flex-col md:flex-row justify-center items-center pb-40">
+        <div className="container flex justify-evenly gap-1">
+          <div className="bg-secondary h-[566px] w-[710px] rounded-[10px] p-10">
+            <p className="text-[30px] text-left text-white ">
               Wir sind ein Team von Experten, die sich auf digitales Marketing
               spezialisiert haben. Wir bieten Ihnen die besten Lösungen für Ihr
               Unternehmen und helfen Ihnen,{" "}
@@ -58,15 +76,44 @@ function App() {
               mit Ihnen zusammenzuarbeiten.
             </p>
           </div>
-        </div>
+          <div className="flex flex-col items-left bg-primary h-[566px] w-[710px] rounded-[10px] p-10">
+            <h2 className="text-[30px] text-secondary text-left">
+              Wir freuen uns darauf,{" "}
+              <strong>mit Ihnen zusammenzuarbeiten.</strong>
+            </h2>
 
-        <div className="h-screen w-full flex justify-center items-center">
-          <div className="container flex justify-evenly gap-1">
-            <div className="bg-secondary h-[566px] w-[710px] rounded-md"></div>
-            <div className="bg-primary h-[566px] w-[710px] rounded-md"></div>
+            <form className="h-full flex flex-col gap-2 my-4">
+              <input
+                type="text"
+                placeholder="Name"
+                className="bg-white rounded-lg px-4 py-2 w-full rounded-lg"
+              />
+              <input
+                type="email"
+                placeholder="E-Mail-Adresse"
+                className="bg-white rounded-lg px-4 py-2 w-full rounded-lg"
+              />
+              <textarea
+                type="text"
+                placeholder="Nachricht"
+                className="bg-white rounded-lg px-4 py-2 w-full h-full rounded-lg"
+              />
+            </form>
+
+            <div className="flex">
+              <input type="checkbox" className="mr-2 " />
+              <p className="text-white text-[16px]">
+                Einwilligung zur Datenverarbeitung*
+              </p>
+            </div>
+            <button className="bg-[#FF6F4B] hover:bg-[#DD5C3B] font-bold text-[16px] text-white px-4 py-2 rounded-lg max-w-[176px] mt-2">
+              Jetzt absenden!
+            </button>
           </div>
         </div>
       </div>
+
+      <Weather />
     </div>
   );
 }
